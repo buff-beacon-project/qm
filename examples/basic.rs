38785e97e46223b12256034b9f1fa513d22fff89
Ross Snyder <rosn0557@colorado.edu>
2021-06-21 12:43:52 -0600

    Added fidelity and concurrence
    Added fidelity and concurrence. Fidelity passed most tests for bell states and maximally mixed states. Deleting diagonalization procedure since these matrices are Hermitian.  Will commit again today but commiting to master today in case I need the diagonalization again.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,58 +1,92 @@
-use num_complex::Complex;
-use std::f64::consts::PI;
-use qm::test;
+use ndarray_linalg::c64;
+use qm::*;
 use ndarray::prelude::*;
-// use ndarray_linalg::trace::*;
-
-pub type ComplexNum = num_complex::Complex<f64>;
-pub type Vec = ndarray::Array1<ComplexNum>;
-pub type SqrMatrix = ndarray::Array2<ComplexNum>;
+// use ndarray_linalg::*;
 
 fn main() {
-  let _message = test();
-  println!("The 20th Fibonacci number is {}", fib(20));
-  println!("Fib(2.1 -3.3i) is {}", fibc(Complex{re:2.1, im:-3.3}));
 
-  let b: Vec = array![Complex{re:2., im:0.},Complex{re:1., im:0.}];
-  let a: Vec = array![Complex{re:3., im:0.},Complex{re:4., im:0.}];
+////////////////////////////////////////////////////////////////////////
+  // Global variables (Bell States, maximally mixed state)
 
-  println!("{}", a.dot(&b));
-  println!("{}",density_matrix(Complex{re:1., im:2.},Complex{re:3., im:4.}))
-}
+  let norm_const = 1./2_f64.sqrt();
+  let bell_phi_plus_coef = array![c64::new(norm_const , 0.0) , c64::new(0.0        , 0.0) , 
+                                  c64::new(0.0        , 0.0) , c64::new(norm_const , 0.0) ];
 
-pub fn fib(n : u64) -> u64 {
- 
- let n_int = n;
- let n_float = n_int as f64;
- let phi: f64 = (1_f64 + 5_f64.sqrt())/2_f64;
- let fib_num: f64 = ((phi.powf(n_float)/5_f64.sqrt()) + 0.5).floor();
- fib_num as u64
-}
+  let bell_phi_minus_coef = array![c64::new(norm_const , 0.0) , c64::new(0.0         , 0.0) , 
+                                   c64::new(0.0        , 0.0) , c64::new(-norm_const , 0.0) ];
 
-pub fn fibc(n: Complex<f64>) -> Complex<f64>{
- 
- let phi: f64 = (1.0 + 5_f64.sqrt())/2.0;
- let ln_phi: ComplexNum = Complex::<f64>::from(phi.ln());
- let phi_to_the_n: ComplexNum = (n*ln_phi).exp();
- let phi_to_the_neg_n: ComplexNum = (-n*ln_phi).exp();
- (phi_to_the_n - ((PI*n).cos())*phi_to_the_neg_n)/5_f64.sqrt()
-}
+  let bell_psi_plus_coef = array![c64::new(0.0        , 0.0) , c64::new(norm_const , 0.0) , 
+                                  c64::new(norm_const , 0.0) , c64::new(0.0        , 0.0) ];
 
-pub fn density_matrix(alpha: ComplexNum, beta: ComplexNum) -> SqrMatrix{
+  let bell_psi_minus_coef = array![c64::new(0.0         , 0.0) , c64::new(norm_const , 0.0) , 
+                                   c64::new(-norm_const , 0.0) , c64::new(0.0 ,        0.0) ];
 
- // let zero_ket: Vec = array![Complex{re:1., im:0.},Complex{re:0., im:0.}];
- // let one_ket: Vec = array![Complex{re:0., im:0.},Complex{re:1., im:0.}];
- // let zero_ket_conj: Vec = array![zero_ket[0].conj(),zero_ket[1].conj()];
- // let one_ket_conj: Vec = array![zero_ket[0].conj(),zero_ket[1].conj()];
- let rho00: ComplexNum = alpha*alpha.conj();
- let rho01: ComplexNum = alpha*beta.conj();
- let rho10: ComplexNum = beta*alpha.conj(); 
- let rho11: ComplexNum = beta*beta.conj();
- array![[rho00,rho01],[rho10,rho11]]
-}
+  let rho_max_mixed_1_qbit = array![ [c64::new(0.5 , 0.0) , c64::new(0.0 , 0.0)] ,
+                                     [c64::new(0.0 , 0.0) , c64::new(0.5 , 0.0)] ];
 
-pub fn find_purity(rho_squared: Array2<f64>)-> f64{
-  let diag = rho_squared.diag();
-  diag.sum()
-  // rho_squared.trace()
+  let rho_max_mixed_2_qbit = array![ [c64::new(0.25 , 0.0) ,  c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0) ] ,
+                                     [c64::new(0.0 , 0.0)  ,  c64::new(0.25 , 0.0) , c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0) ] ,
+                                     [c64::new(0.0 , 0.0)  ,  c64::new(0.0 , 0.0)  , c64::new(0.25 , 0.0) , c64::new(0.0 , 0.0) ] ,
+                                     [c64::new(0.0 , 0.0)  ,  c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0)  , c64::new(0.25 , 0.0)] ];
+////////////////////////////////////////////////////////////////////////
+  //Fibonacci function tests
+
+  // println!("The 20th Fibonacci number is {}", fib(20));
+  // println!("Fib(2.1 -3.3i) is {}", fibc(Complex::new(2.1, im:-3.3}));
+
+//////////////////////////////////////////////////////////////////////////  
+  //Density Matrix tests
+
+  let rho_bell_phi_plus = create_dens_matrix(bell_phi_plus_coef);
+  let rho_bell_phi_minus = create_dens_matrix(bell_phi_minus_coef);
+  let rho_bell_psi_plus = create_dens_matrix(bell_psi_plus_coef);
+  let rho_bell_psi_minus = create_dens_matrix(bell_psi_minus_coef);
+
+  let rho_bell_psi_minus_2 = rho_bell_psi_minus.clone();
+  let rho_bell_psi_minus_3 = rho_bell_psi_minus_2.clone();
+
+  // println!("dens matrix for phi_+ = \n {} \n", rho_bell_phi_plus);
+  // println!("dens matrix for phi_- = \n {} \n", rho_bell_phi_minus);
+  // println!("dens matrix for psi_+ = \n {} \n", rho_bell_psi_plus);
+  // println!("dens matrix for psi_- = \n {} \n", rho_bell_psi_minus)
+
+//////////////////////////////////////////////////////////////////////////  
+  //Purity test
+  
+  println!("Dens matrix for a maximally mixed state {}: \n",rho_max_mixed_1_qbit);
+  let rho_a_max_mixed_1_qbit = rho_max_mixed_1_qbit.clone();
+  let rho_sqrd_1_qbit = find_dens_matrix_sqrd(rho_a_max_mixed_1_qbit);
+  let rho_a_sqrd_1_qbit = rho_sqrd_1_qbit.clone();
+  let rho_b_sqrd_1_qbit = rho_a_sqrd_1_qbit.clone();
+  println!("Dens matrix squared is \n {} \n",rho_sqrd_1_qbit);
+  println!("The purity is {} \n", find_purity(rho_a_sqrd_1_qbit));
+  println!("The purity ranges from {} to {} \n", 1./find_dim(rho_b_sqrd_1_qbit), 1);
+
+  // println!("Dens matrix for a pure state {}: \n",rho_bell_phi_plus);
+  // let rho_bell_2 = rho_bell_phi_plus.clone();
+  // let rho_bell_sqrd = find_dens_matrix_sqrd(rho_bell_2);
+  // let rho_bell_sqrd_2 = rho_bell_sqrd.clone();
+  // let rho_bell_sqrd_3 = rho_bell_sqrd_2.clone();
+  // println!("Dens matrix squared is \n {} \n",rho_bell_sqrd);
+  // println!("The purity is {} \n", find_purity(rho_bell_sqrd_2));
+  // println!("The purity ranges from {} to {}", 1./find_dim(rho_bell_sqrd_3), 1)  
+
+//////////////////////////////////////////////////////////////////////////  
+  //Fidelity test
+
+  let rho_max_a_2_qbit = rho_max_mixed_2_qbit.clone();
+  let rho_max_b_2_qbit = rho_max_a_2_qbit.clone();
+  let rho_max_c_2_qbit = rho_max_b_2_qbit.clone();
+  let rho_max_d_2_qbit = rho_max_c_2_qbit.clone();
+  let rho_max_e_2_qbit = rho_max_d_2_qbit.clone();
+
+
+  println!("The fidelity for a maximally mixed state with itself is {} \n",fidelity(rho_max_mixed_2_qbit,rho_max_a_2_qbit));
+  println!("The fidelity for a maximally mixed state with phi_+ is {} \n",fidelity(rho_max_b_2_qbit,rho_bell_phi_plus));
+  println!("The fidelity for a maximally mixed state with phi_- is {} \n",fidelity(rho_max_c_2_qbit,rho_bell_phi_minus));  
+  println!("The fidelity for a maximally mixed state with psi_+  is {} \n",fidelity(rho_max_d_2_qbit,rho_bell_psi_plus));  
+  println!("The fidelity for a maximally mixed state with psi_-  is {} \n",fidelity(rho_max_e_2_qbit,rho_bell_psi_minus));  
+
+
+  println!("The fidelity for psi_- with psi_-  is {} \n",fidelity(rho_bell_psi_minus_2,rho_bell_psi_minus_3)); 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,146 @@
-use num_complex::Complex;
 use std::f64::consts::PI;
+// use std::cmp;
+use ndarray::prelude::*;
+use ndarray_linalg::*;
 
-pub fn test() -> String {
-  "Hello world".into()
-}
+pub type Complex = num_complex::Complex<f64>;
+pub type Vec = ndarray::Array1<c64>;
+pub type MatrixC64 = ndarray::Array2<c64>;
+pub type MatrixF64 = ndarray::Array2<f64>;
 
 /// Get the "n"th fibonacci number using integers only.
 pub fn fib(n : u64) -> u64 {
  
- let n_int = n;
- let n_float = n_int as f64;
- let phi: f64 = (1_f64 + 5_f64.sqrt())/2_f64;
- let fib_num: f64 = ((phi.powf(n_float)/5_f64.sqrt()) + 0.5).floor();
+ let n_float = n as f64;
+ let phi = ( 1. + 5_f64.sqrt() ) / 2.;
+ let fib_num = ( (phi.powf(n_float) / 5_f64.sqrt() ) + 0.5 ).floor();
+
  fib_num as u64
+
 }
 
-/// Get the "n"th fibonacci number using complex numbers. For example, n = Complex{re:2.1, im:-3.3}.
-pub fn fibc(n: Complex<f64>) -> Complex<f64>{
+/// Get the "n"th fibonacci number using complex numbers. For example, n = Complex::new(2.1, -3.3).
+pub fn fibc(n: Complex) -> Complex{
  
- let phi: f64 = (1.0 + 5_f64.sqrt())/2.0;
- let phi_to_the_n: Complex<f64> = (n*Complex::<f64>::from(phi.ln())).exp();
- let phi_to_the_neg_n: Complex<f64> = (-n*Complex::<f64>::from(phi.ln())).exp();
- (phi_to_the_n - ((PI*n).cos())*phi_to_the_neg_n)/5_f64.sqrt()
+ let phi: f64 = ( 1. + 5_f64.sqrt() ) / 2.;
+ let ln_phi = Complex::from( phi.ln() );
+ let phi_to_the_n = (n * ln_phi).exp();
+ let phi_to_the_neg_n = (-n * ln_phi).exp();
+
+ (phi_to_the_n - ( (PI * n).cos() ) * phi_to_the_neg_n) / 5_f64.sqrt()
+
 }
+
+pub fn create_dens_matrix(coefs: Vec) -> MatrixC64{
+
+  let coef_num = coefs.len() as i32;
+  let dens_mat_len = coef_num as usize;
+  let mut dens_matrix = MatrixC64::zeros((dens_mat_len,dens_mat_len).f());
+
+  let mut ctr_i = 0;
+
+  for _coef in 0..coef_num{
+    let mut ctr_j = 0;
+    for _coef_2 in 0..coef_num{
+      dens_matrix[ [ctr_i , ctr_j] ] = coefs[ctr_i]*(coefs[ctr_j]).conj();
+      ctr_j += 1;
+    }
+  ctr_i +=1;
+  }
+  
+  dens_matrix
+}
+
+
+pub fn find_dens_matrix_sqrd(rho: MatrixC64) -> MatrixC64{
+  let rho_sqrd = rho.dot(&rho); 
+  rho_sqrd
+}
+
+pub fn find_purity(rho_sqrd: MatrixC64)-> f64{
+  let purity = rho_sqrd.trace().unwrap();
+  purity.re
+}
+pub fn find_dim(rho_sqrd: MatrixC64)-> f64{
+  let shape = rho_sqrd.dim();
+  shape.1 as f64
+}
+
+// pub fn find_sqr_root_of_matrix(rho: MatrixC64) -> MatrixC64 {
+
+// //Hermitian matrices are always diagonalizable, so we can express rho = S*D*(S^-1),
+// //so the sqrt(rho) = S*(D^0.5)*(S^-1).
+
+//   let (eigs, vecs) = rho.eigh().unwrap();
+//   let (eig_0,eig_1) = (eigs[0].re, eigs[1].re);
+//   let (vecs_0, vecs_1, vecs_2, vecs_3) = (vecs[[0 , 0]].re , vecs[[0 , 1]].re , vecs[[1 , 0]].re , vecs[[1 , 1]].re);
+
+//   let mat_s = array![ [ vecs_0 , vecs_1 ], 
+//                       [ vecs_2 , vecs_3 ] ];
+
+//   let mat_d = array![ [ eig_0 ,   0.0   ], 
+//                       [   0.0 , eig_1   ] ];
+
+//   let mat_s_inv = mat_s.inv().unwrap();
+
+// //ssqrt only applies to real, Hermitian matrices. rho may have complex off-diagonal terms.
+
+//   let sqrt_mat_d = mat_d.ssqrt(UPLO::Lower).unwrap();
+//   let sqrt_product = mat_s.dot(&sqrt_mat_d).dot(&mat_s_inv);
+
+// }
+
+pub fn fidelity(rho: MatrixC64, sigma: MatrixC64) -> f64 {
+
+  let sqrt_rho = rho.ssqrt(UPLO::Lower).unwrap();
+  let product = sqrt_rho.dot(&sigma).dot(&sqrt_rho);
+  let sqrt_product = product.ssqrt(UPLO::Lower).unwrap();
+  (sqrt_product.trace().unwrap()).re
+
+  //Test code for debugging
+
+  // let s_d_s_inv = mat_s.dot(&mat_d).dot(&mat_s_inv);
+  // println!("vecs = {} \n, mat_s = {} \n",vecs,mat_s);
+  // println!("mat_d = {} \n",mat_d);
+  // println!("sqrt_mat_d = {} \n", sqrt_mat_d);
+  // println!("s_d_s_inv = {} \n, product = {} \n",s_d_s_inv, product);
+  // println!("Square root of product squared = {}",sqrt_product.dot(&sqrt_product));
+}
+
+// fn sort<A, T>(mut array: A) -> A
+// where
+//     A: AsMut<[T]>,
+//     T: Ord,
+// {
+//     let slice = array.as_mut();
+//     slice.sort();
+//     array
+// }
+
+// pub fn find_concurrence(rho: MatrixC64) -> f64{
+
+//   let pauli_y = array![ [  c64::new(0. , 0.)  ,  c64::new(0. , 0.) , c64::new(0. , 0.) ,  c64::new(-1. , 0.)  ] , 
+//                         [  c64::new(0. , 0.)  ,  c64::new(0. , 0.) , c64::new(1. , 0.) ,  c64::new(0. , 0.)   ] ,
+//                         [  c64::new(0. , 0.)  ,  c64::new(1. , 0.) , c64::new(0. , 0.) ,  c64::new(0. , 0.)   ] ,
+//                         [  c64::new(-1. , 0.) ,  c64::new(0. , 0.) , c64::new(0. , 0.) ,  c64::new(0. , 0.)   ] ];
+
+//   let sqrt_rho = rho.ssqrt(UPLO::Lower).unwrap();
+//   let rho_star = rho.mapv(|rho| rho.conj());
+//   let rho_tilde = pauli_y.dot(&rho_star).dot(&pauli_y);
+
+//   let product = sqrt_rho.dot(&rho_tilde).dot(&sqrt_rho);
+//   let sqrt_product = product.ssqrt(UPLO::Lower).unwrap();
+//   let (eigvals, _eigvecs) = sqrt_product.eigh(UPLO::Lower).unwrap();
+  
+//   // let mut sorted_eigvals = sort(eigvals);
+//   println!("eigs = {}",eigvals);
+//   // cmp::max(0., eigvals[3] - eigvals[2] - eigvals[1] - eigvals[0])
+
+//   1.1
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
 mod tests {
@@ -30,10 +148,12 @@ mod tests {
 
   #[test]
   fn test_fibc() {
-    let actual = fibc(Complex{re:2.1, im:-3.3});
-    let expected = Complex{re:842.107900737978028312629, im: -2448.9411071123121308522611};
-    assert!((actual - expected).norm_sqr() <= 1e-6);
-    println!("{}",actual)
+    let actual = fibc(Complex::new(2.1, -3.3));
+    // let expected = Complex::new( 842.107900737978028312629,  -2448.9411071123121308522611};
+    let expected = Complex::new(2.1, 4.4);
+    
+    assert!( (actual - expected).norm_sqr() <= 1e-6 );
+    println!("{}" , actual)
   }
 }
 
