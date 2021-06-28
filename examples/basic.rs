eb2c1793418c31491e5155652e170ff9f8d3adda
Ross Snyder <rosn0557@colorado.edu>
2021-06-28 10:30:12 -0600

    Added negativity, log negativity, trace norm
    Cleaned up the library a little more and added a few more functions.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,12 +1,12 @@
 use ndarray_linalg::*;
-use std::f32::consts::PI;
+use std::f64::consts::PI;
 use qm::*;
 use ndarray::prelude::*;
 
 fn main() {
 
 ////////////////////////////////////////////////////////////////////////
-  // Global variables (Bell States, maximally mixed state)
+  //Global variables (Bell States, maximally mixed state)
 
   // let norm_const = 1./2_f64.sqrt();
   // let bell_phi_plus_coef = array![c64::new(norm_const , 0.0) , c64::new(0.0        , 0.0) , 
@@ -21,13 +21,20 @@ fn main() {
   // let bell_psi_minus_coef = array![c64::new(0.0         , 0.0) , c64::new(norm_const , 0.0) , 
   //                                  c64::new(-norm_const , 0.0) , c64::new(0.0 ,        0.0) ];
 
-  // let rho_max_mixed_1_qbit = array![ [c64::new(0.5 , 0.0) , c64::new(0.0 , 0.0)] ,
-  //                                    [c64::new(0.0 , 0.0) , c64::new(0.5 , 0.0)] ];
+  // // let rho_max_mixed_1_qbit = array![ [c64::new(0.5 , 0.0) , c64::new(0.0 , 0.0)] ,
+  // //                                    [c64::new(0.0 , 0.0) , c64::new(0.5 , 0.0)] ];
 
-  // let rho_max_mixed_2_qbit = array![ [c64::new(0.25 , 0.0) ,  c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0) ] ,
-  //                                    [c64::new(0.0 , 0.0)  ,  c64::new(0.25 , 0.0) , c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0) ] ,
-  //                                    [c64::new(0.0 , 0.0)  ,  c64::new(0.0 , 0.0)  , c64::new(0.25 , 0.0) , c64::new(0.0 , 0.0) ] ,
-  //                                    [c64::new(0.0 , 0.0)  ,  c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0)  , c64::new(0.25 , 0.0)] ];
+  let rho_max_mixed_2_qbit = array![ [c64::new(0.25 , 0.0) ,  c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0) ] ,
+                                     [c64::new(0.0 , 0.0)  ,  c64::new(0.25 , 0.0) , c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0) ] ,
+                                     [c64::new(0.0 , 0.0)  ,  c64::new(0.0 , 0.0)  , c64::new(0.25 , 0.0) , c64::new(0.0 , 0.0) ] ,
+                                     [c64::new(0.0 , 0.0)  ,  c64::new(0.0 , 0.0)  , c64::new(0.0 , 0.0)  , c64::new(0.25 , 0.0)] ];
+
+  let test_mat = array![             [c64::new(1.0 , 0.0) , c64::new(2.0 , 0.0)   , c64::new(3.0 , 0.0)  , c64::new(4.0 , 0.0) ] ,
+                                     [c64::new(5.0 , 0.0) , c64::new(6.0 , 0.0)   , c64::new(7.0 , 0.0)  , c64::new(8.0 , 0.0) ] ,
+                                     [c64::new(9.0 , 0.0) , c64::new(10.0 , 0.0)  , c64::new(11.0 , 0.0) , c64::new(12.0 , 0.0) ] ,
+                                     [c64::new(13.0 , 0.0) , c64::new(14.0 , 0.0) , c64::new(15.0 , 0.0) , c64::new(16.0 , 0.0)] ];
+  // let theta = PI*(60./180.);
+  // let psi_part_entangled = array![c64::new(theta.cos(), 0.0), c64::new(0.0, 0.0), c64::new(0.0, 0.0), c64::new(theta.sin(), 0.0)];
 
 ////////////////////////////////////////////////////////////////////////
   //Fibonacci function tests
@@ -43,65 +50,46 @@ fn main() {
   // let rho_bell_psi_plus = create_dens_matrix(bell_psi_plus_coef);
   // let rho_bell_psi_minus = create_dens_matrix(bell_psi_minus_coef);
 
-
+  // let rho_part_entangled = create_dens_matrix(psi_part_entangled);
 
   // println!("dens matrix for phi_+ = \n {} \n", rho_bell_phi_plus);
   // println!("dens matrix for phi_- = \n {} \n", rho_bell_phi_minus);
   // println!("dens matrix for psi_+ = \n {} \n", rho_bell_psi_plus);
-  // println!("dens matrix for psi_- = \n {} \n", rho_bell_psi_minus)
+  // println!("dens matrix for psi_- = \n {} \n", rho_bell_psi_minus);
 
+  // println!("pauli spin = {}",pauli_tensor);
 //////////////////////////////////////////////////////////////////////////  
   //Purity test
   
   // println!("Dens matrix for a maximally mixed state {}: \n",rho_max_mixed_1_qbit);
   // let rho_a_max_mixed_1_qbit = rho_max_mixed_1_qbit.clone();
   // let rho_sqrd_1_qbit = find_dens_matrix_sqrd(rho_a_max_mixed_1_qbit);
-  // let rho_a_sqrd_1_qbit = rho_sqrd_1_qbit.clone();
-  // let rho_b_sqrd_1_qbit = rho_a_sqrd_1_qbit.clone();
   // println!("Dens matrix squared is \n {} \n",rho_sqrd_1_qbit);
-  // println!("The purity is {} \n", find_purity(rho_a_sqrd_1_qbit));
-  // println!("The purity ranges from {} to {} \n", 1./find_dim(rho_b_sqrd_1_qbit), 1);
+  // println!("The purity is {} \n", find_purity(rho_sqrd_1_qbit.clone()));
+  // println!("The purity ranges from {} to {} \n", 1./find_dim(rho_sqrd_1_qbit.clone()), 1);
 
   // println!("Dens matrix for a pure state {}: \n",rho_bell_phi_plus);
   // let rho_bell_2 = rho_bell_phi_plus.clone();
   // let rho_bell_sqrd = find_dens_matrix_sqrd(rho_bell_2);
-  // let rho_bell_sqrd_2 = rho_bell_sqrd.clone();
-  // let rho_bell_sqrd_3 = rho_bell_sqrd_2.clone();
   // println!("Dens matrix squared is \n {} \n",rho_bell_sqrd);
-  // println!("The purity is {} \n", find_purity(rho_bell_sqrd_2));
-  // println!("The purity ranges from {} to {}", 1./find_dim(rho_bell_sqrd_3), 1);
+  // println!("The purity is {} \n", find_purity(rho_bell_sqrd.clone()));
+  // println!("The purity ranges from {} to {}", 1./find_dim(rho_bell_sqrd.clone()), 1);
 
 //////////////////////////////////////////////////////////////////////////  
   //Fidelity test
 
-  // let rho_max_a_2_qbit = rho_max_mixed_2_qbit.clone();
-  // let rho_max_b_2_qbit = rho_max_a_2_qbit.clone();
-  // let rho_max_c_2_qbit = rho_max_b_2_qbit.clone();
-  // let rho_max_d_2_qbit = rho_max_c_2_qbit.clone();
-  // let rho_max_e_2_qbit = rho_max_d_2_qbit.clone();
+  // println!("The fidelity for a maximally mixed state with itself is {} \n",fidelity(rho_max_mixed_2_qbit.clone(),rho_max_mixed_2_qbit.clone()));
+  // println!("The fidelity for a maximally mixed state with phi_+ is {} \n",fidelity(rho_max_mixed_2_qbit.clone(),rho_bell_phi_plus.clone()));
+  // println!("The fidelity for a maximally mixed state with phi_- is {} \n",fidelity(rho_max_mixed_2_qbit.clone(),rho_bell_phi_minus.clone()));  
+  // println!("The fidelity for a maximally mixed state with psi_+  is {} \n",fidelity(rho_max_mixed_2_qbit.clone(),rho_bell_psi_plus.clone()));  
+  // println!("The fidelity for a maximally mixed state with psi_-  is {} \n",fidelity(rho_max_mixed_2_qbit.clone(),rho_bell_psi_minus.clone()));  
 
-  // let rho_2_bell_psi_minus = rho_bell_psi_minus.clone();
-  // let rho_3_bell_psi_minus = rho_2_bell_psi_minus.clone();
-  // let rho_4_bell_psi_minus = rho_3_bell_psi_minus.clone();
-
-  // let rho_2_bell_psi_plus = rho_bell_psi_plus.clone();
-  // let rho_3_bell_psi_plus = rho_2_bell_psi_plus.clone();
-
-  // println!("The fidelity for a maximally mixed state with itself is {} \n",fidelity(rho_max_mixed_2_qbit,rho_max_a_2_qbit));
-  // println!("The fidelity for a maximally mixed state with phi_+ is {} \n",fidelity(rho_max_b_2_qbit,rho_bell_phi_plus));
-  // println!("The fidelity for a maximally mixed state with phi_- is {} \n",fidelity(rho_max_c_2_qbit,rho_bell_phi_minus));  
-  // println!("The fidelity for a maximally mixed state with psi_+  is {} \n",fidelity(rho_max_d_2_qbit,rho_bell_psi_plus));  
-  // println!("The fidelity for a maximally mixed state with psi_-  is {} \n",fidelity(rho_max_e_2_qbit,rho_bell_psi_minus));  
-
-  // println!("The fidelity for psi_- with psi_-  is {} \n",fidelity(rho_2_bell_psi_minus,rho_3_bell_psi_minus));
-  // println!("The fidelity for psi_+ with psi_-  is {} \n",fidelity(rho_2_bell_psi_plus,rho_4_bell_psi_minus));   
+  // println!("The fidelity for psi_- with psi_-  is {} \n",fidelity(rho_bell_psi_minus.clone(),rho_bell_psi_minus.clone()));
+  // println!("The fidelity for psi_+ with psi_-  is {} \n",fidelity(rho_bell_psi_plus.clone(),rho_bell_psi_minus.clone()));   
+  // println!("Fidelity of cos({:.4})|00> + sin({:.4})|11> is {}", theta, theta, fidelity_test(rho_part_entangled.clone(), rho_part_entangled.clone()));   
 
 //////////////////////////////////////////////////////////////////////////  
   //Concurrence Tests
-
-  // let coef_test = array![c64::new(1./8_f64.sqrt(), 0.0), c64::new(0.75_f64.sqrt(), (data)0.0), c64::new(1./8_f64.sqrt(), 0.0), c64::new(0.75_f64.sqrt(), 0.0)];
-  // let test_mat = create_dens_matrix(coef_test);
-  // println!("test_mat = {}", test_mat);
   
   // println!("Concurrence of phi_+ is {}", find_concurrence(rho_bell_phi_plus));
   // println!("Concurrence of phi_- is {}", find_concurrence(rho_bell_phi_minus));
@@ -109,30 +97,11 @@ fn main() {
   // println!("Concurrence of psi_- is {}", find_concurrence(rho_bell_psi_minus));
 
   // println!("Concurrence of mixed state is {}", find_concurrence(rho_max_mixed_2_qbit));
+  // println!("Concurrence of cos({:.4})|00> + sin({:.4})|11> is {}", theta, theta,find_concurrence(rho_part_entangled));  
 
-  let theta = PI*(75./180.);
-  let psi_part_entangled = array![c32::new(theta.cos(), 0.0), c32::new(0.0, 0.0), c32::new(0.0, 0.0), c32::new(theta.sin(), 0.0)];
-  let rho_part_entangled = create_dens_matrix(psi_part_entangled);
+////////////////////////////////////////////////////////////////////////////
+  //partial transpose test
 
-  // let rho_part_entangled_2 = array![ [  rho_part_entangled[[0,0]] , rho_part_entangled[[1,0]] , rho_part_entangled[[2,0]] , rho_part_entangled[[0,3]]   ] , 
-  //                                      [  rho_part_entangled[[0,1]] , rho_part_entangled[[1,1]] , rho_part_entangled[[2,1]] , rho_part_entangled[[3,1]]   ] ,
-  //                                      [  rho_part_entangled[[0,2]] , rho_part_entangled[[1,2]] , rho_part_entangled[[2,2]] , rho_part_entangled[[3,2]]   ] ,
-  //                                      [  rho_part_entangled[[0,3]] , rho_part_entangled[[1,3]] , rho_part_entangled[[2,3]] , rho_part_entangled[[3,3]]   ] ];
+  println!("{}", find_partial_transpose(test_mat))
 
-  // let psi_00 = array![c32::new(theta.cos(), 0.0), c32::new(0.0, 0.0), c32::new(0.0, 0.0), c32::new(0.0, 0.0)];
-  // let psi_11 = array![c32::new(0.0, 0.0), c32::new(0.0, 0.0), c32::new(0.0, 0.0), c32::new(theta.sin(), 0.0)];  
-
-  // let rho_00 = create_dens_matrix(psi_00);  
-  // let rho_11 = create_dens_matrix(psi_11);  
-  // let rho_mix = create_dens_matrix(psi_00 + psi_11);
-  // let rho_a = rho_part_entangled.mapv(|rho_part_entangled| (rho_part_entangled.re * 1000.).round() / 1000.0);
-  // let rho_b = rho_part_entangled.mapv(|rho_part_entangled| (rho_part_entangled.im * 1000.).round() / 1000.0);
-  // let rho_c = rho_a.map(|f| c32::new(*f, 0.0));
-  // let rho_d = rho_b.map(|g| c32::new(0.0, *g));
-  // let rho_rounded = rho_c + rho_d;
-  // println!("rounded rho is \n {}\n ", rho_rounded);
-  // println!("{}",rho_part_entangled);
-
-  println!("Concurrence of partially entangled state is {}", find_concurrence(rho_part_entangled));  
-  // println!("Concurrence of rho_mix is {}", find_concurrence(rho_mix));  
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,10 @@
 use std::f64::consts::PI;
-// use std::vec::*;
-// use std::cmp;
 use ndarray::prelude::*;
 use ndarray_linalg::*;
 
 pub type Complex = num_complex::Complex<f64>;
 pub type VecC64 = ndarray::Array1<c64>;
-pub type VecC32 = ndarray::Array1<c32>;
 pub type MatrixC64 = ndarray::Array2<c64>;
-pub type MatrixC32 = ndarray::Array2<c32>;
-pub type MatrixF64 = ndarray::Array2<f64>;
-pub type MatrixF32 = ndarray::Array2<f32>;
 
 /// Get the "n"th fibonacci number using integers only.
 pub fn fib(n : u64) -> u64 {
@@ -35,18 +29,18 @@ pub fn fibc(n: Complex) -> Complex{
 
 }
 
-pub fn create_dens_matrix(coefs: VecC32) -> MatrixC32{
+pub fn create_dens_matrix(coefs: VecC64) -> MatrixC64{
 
   let coef_num = coefs.len() as i32;
-  let dens_mat_len = coef_num as usize;
-  let mut dens_matrix = MatrixC32::zeros((dens_mat_len,dens_mat_len).f());
+  let dens_matrix_len = coef_num as usize;
+  let mut dens_matrix = MatrixC64::zeros((dens_matrix_len , dens_matrix_len).f());
 
   let mut ctr_i = 0;
 
   for _coef in 0..coef_num{
     let mut ctr_j = 0;
     for _coef_2 in 0..coef_num{
-      dens_matrix[ [ctr_i , ctr_j] ] = coefs[ctr_i]*(coefs[ctr_j]).conj();
+      dens_matrix[ [ctr_i , ctr_j] ] = coefs[ctr_i] * ( coefs[ctr_j] ).conj();
       ctr_j += 1;
     }
   ctr_i +=1;
@@ -55,136 +49,185 @@ pub fn create_dens_matrix(coefs: VecC32) -> MatrixC32{
   dens_matrix
 }
 
-
-pub fn find_dens_matrix_sqrd(rho: MatrixC32) -> MatrixC32{
+pub fn find_dens_matrix_sqrd(rho: MatrixC64) -> MatrixC64{
   let rho_sqrd = rho.dot(&rho); 
   rho_sqrd
 }
 
-pub fn find_purity(rho_sqrd: MatrixC32)-> f32{
+pub fn find_purity(rho_sqrd: MatrixC64)-> f64{
   let purity = rho_sqrd.trace().unwrap();
   purity.re
 }
-pub fn find_dim(rho_sqrd: MatrixC32)-> f32{
+pub fn find_dim(rho_sqrd: MatrixC64)-> i32 {
   let shape = rho_sqrd.dim();
-  shape.1 as f32
+  shape.1 as i32
 }
 
-pub fn fidelity(rho: MatrixC32, sigma: MatrixC32) -> f32 {
+pub fn fidelity(rho: MatrixC64, sigma: MatrixC64) -> f64 {
 
-  let sqrt_rho = rho.ssqrt(UPLO::Lower).unwrap();
+  let (matrix_d, matrix_s) = rescale_neg_eigvals(rho);
+  let sqrt_rho = find_sqr_root_of_matrix(matrix_d, matrix_s);
   let product = sqrt_rho.dot(&sigma).dot(&sqrt_rho);
-  let sqrt_product = product.ssqrt(UPLO::Lower).unwrap();
+  let (matrix_d_product, matrix_s_product) = rescale_neg_eigvals(product);  
+  let sqrt_product = find_sqr_root_of_matrix(matrix_d_product, matrix_s_product);
   (sqrt_product.trace().unwrap()).re
 }
 
-////////////////////////////////////////////////////////
-// pub fn find_concurrence(rho: MatrixC32) -> f32{
+// pub fn fidelity_test(rho: MatrixC64, sigma: MatrixC64) -> f64 {
 
-//   let pauli_y = array![ [  c32::new(0. , 0.)  ,  c32::new(0. , 0.) , c32::new(0. , 0.) ,  c32::new(-1. , 0.)  ] , 
-//                         [  c32::new(0. , 0.)  ,  c32::new(0. , 0.) , c32::new(1. , 0.) ,  c32::new(0. , 0.)   ] ,
-//                         [  c32::new(0. , 0.)  ,  c32::new(1. , 0.) , c32::new(0. , 0.) ,  c32::new(0. , 0.)   ] ,
-//                         [  c32::new(-1. , 0.) ,  c32::new(0. , 0.) , c32::new(0. , 0.) ,  c32::new(0. , 0.)   ] ];
-//   println!("1) rho is \n {} \n ", rho);
-  
-//   // if rho[[0 , 3]]==rho[[3,0]]{
-//   //     println!("They are equal")};
-
-
-//   let sqrt_rho_lower = rho.ssqrt(UPLO::Lower);
-//   println!("2) sqrt_root_rho_lower = \n {:?}\n ",sqrt_rho_lower);
-
-//   // println!("rho1 = {}, rho2 = {}",rho[[0 , 3]], rho[[3,0]]);
-
-//   let sqrt_rho = sqrt_rho_lower.unwrap();
-//   // println!("3) sqrt_root_rho_lower = \n {:?}\n ",sqrt_rho_lower);
-//   println!("3) sqrt_root_rho = \n {:?}\n ",sqrt_rho);  
-//   let rho_star = rho.mapv(|rho| rho.conj());
-
-//   let rho_tilde = pauli_y.dot(&rho_star).dot(&pauli_y);
-
-//   let product = sqrt_rho.dot(&rho_tilde).dot(&sqrt_rho);
-//   println!("4) product = \n {:?}\n ",product);  
-//   let sqrt_product = product.ssqrt(UPLO::Lower).unwrap();
-//   println!("5) sqrt_product = \n {:?}\n ",sqrt_product); 
-
-//   let (eigvals, _eigvecs) = sqrt_product.eigh(UPLO::Lower).unwrap();
-  
-//   println!("eigs = {}",eigvals);
-//   0_f32.max(eigvals[3] - eigvals[2] - eigvals[1] - eigvals[0])
+//   let (eigs_old, vecs_old) = rho.eigh(UPLO::Lower).unwrap();
+//   println!("Eigenvalues of old matrix are {}",eigs_old);
+//   println!("Eigenvectors of old matrix are {}",vecs_old);  
+//   println!("rho_old is {}",rho);
+//   let rho_rescaled = rescale_neg_eigvals(rho);
+//   println!("rho_rescaled is {}",rho_rescaled);
+//   let (eigs, vecs) = rho_rescaled.eigh(UPLO::Lower).unwrap();
+//   println!("Eigenvalues of rescaled matrix are {}",eigs);
+//   println!("Eigenvectors of rescaled matrix are {}",vecs);
+//   let sqrt_rho = rho_rescaled.ssqrt(UPLO::Lower).unwrap();
+//   let product = sqrt_rho.dot(&sigma).dot(&sqrt_rho);
+//   let product_rescaled = rescale_neg_eigvals(product);  
+//   let sqrt_product = product_rescaled.ssqrt(UPLO::Lower).unwrap();
+//   (sqrt_product.trace().unwrap()).re
 // }
-/////////////////////////////////////////////////////////////////
-pub fn find_concurrence(rho: MatrixC32) -> f32{
 
-  let pauli_y = array![ [  c32::new(0. , 0.)  ,  c32::new(0. , 0.) , c32::new(0. , 0.) ,  c32::new(-1. , 0.)  ] , 
-                        [  c32::new(0. , 0.)  ,  c32::new(0. , 0.) , c32::new(1. , 0.) ,  c32::new(0. , 0.)   ] ,
-                        [  c32::new(0. , 0.)  ,  c32::new(1. , 0.) , c32::new(0. , 0.) ,  c32::new(0. , 0.)   ] ,
-                        [  c32::new(-1. , 0.) ,  c32::new(0. , 0.) , c32::new(0. , 0.) ,  c32::new(0. , 0.)   ] ];
-  println!("1) rho is \n {} \n ", rho);
+pub fn find_concurrence(rho: MatrixC64) -> f64{
 
-  let sqrt_rho = find_sqr_root_of_matrix(rho.clone());
-  println!("3) sqrt_root_rho = \n {:?}\n ",sqrt_rho);  
+  let pauli_y = array![ [  c64::new(0. , 0.)  ,  c64::new(0. , 0.) , c64::new(0. , 0.) ,  c64::new(-1. , 0.)  ] , 
+                        [  c64::new(0. , 0.)  ,  c64::new(0. , 0.) , c64::new(1. , 0.) ,  c64::new(0. , 0.)   ] ,
+                        [  c64::new(0. , 0.)  ,  c64::new(1. , 0.) , c64::new(0. , 0.) ,  c64::new(0. , 0.)   ] ,
+                        [  c64::new(-1. , 0.) ,  c64::new(0. , 0.) , c64::new(0. , 0.) ,  c64::new(0. , 0.)   ] ];
+
   let rho_star = rho.mapv(|rho| rho.conj());
+  let (matrix_d, matrix_s) = rescale_neg_eigvals(rho.clone());
+  let sqrt_rho = find_sqr_root_of_matrix(matrix_d, matrix_s);
 
   let rho_tilde = pauli_y.dot(&rho_star).dot(&pauli_y);
 
-  let product = sqrt_rho.dot(&rho_tilde).dot(&sqrt_rho);
-  println!("4) product = \n {:?}\n ",product);  
+  let matrix_prod = sqrt_rho.dot(&rho_tilde).dot(&sqrt_rho);
+  let (matrix_d_prod, matrix_s_prod) = rescale_neg_eigvals(matrix_prod);
+  let sqrt_prod = find_sqr_root_of_matrix(matrix_d_prod, matrix_s_prod);
 
-  let sqrt_product = find_sqr_root_of_matrix(product);
-  println!("5) sqrt_product = \n {:?}\n ",sqrt_product); 
-
-  let (eigvals, _eigvecs) = sqrt_product.eigh(UPLO::Lower).unwrap();
+  let (eigvals, _eigvecs) = sqrt_prod.eigh(UPLO::Lower).unwrap();
   
-  println!("eigs = {}",eigvals);
-  0_f32.max(eigvals[3] - eigvals[2] - eigvals[1] - eigvals[0])
+  0_f64.max(eigvals[3] - eigvals[2] - eigvals[1] - eigvals[0])
 }
 
-////////////////////////////////////////////////////////////////
-pub fn find_sqr_root_of_matrix(rho: MatrixC32) -> MatrixC32 {
+pub fn find_negativity(rho: MatrixC64) -> f64{
+
+  let trace_norm = find_trace_norm(rho);
+  (trace_norm - 1.)/2.
+}
+
+pub fn find_trace_norm(rho: MatrixC64) -> f64{
+
+  let rho_partial_transpose = find_partial_transpose(rho);
+  let rho_partial_transpose_star   = rho_partial_transpose.mapv(|rho_partial_transpose| rho_partial_transpose.conj());
+  let rho_partial_transpose_dagger = rho_partial_transpose_star.t();
+  
+  let inner_product = (rho_partial_transpose_dagger).dot(&rho_partial_transpose);
+  let (matrix_d, matrix_s) = rescale_neg_eigvals(inner_product);
+  let partial_transpose_norm = find_sqr_root_of_matrix(matrix_d, matrix_s); 
+  let trace_norm =  partial_transpose_norm.trace().unwrap();
+  trace_norm.re
+}
+
+pub fn find_partial_transpose(rho: MatrixC64) -> MatrixC64{
+
+  let dim = find_dim(rho.clone()) as usize;
+  let mut partial_transpose_matrix = MatrixC64::zeros((dim , dim).f());
+
+  let upper_left_block  = rho.slice(s! [0..(dim / 2)   , 0..(dim / 2)  ] );
+  let upper_right_block = rho.slice(s! [0..(dim / 2)   , (dim / 2)..dim] );
+  let lower_left_block  = rho.slice(s! [(dim / 2)..dim , 0..(dim / 2)  ] );
+  let lower_right_block = rho.slice(s! [(dim / 2)..dim , (dim / 2)..dim] );
+
+  let upper_right_block_transpose = upper_right_block.t();
+  let lower_left_block_transpose = lower_left_block.t();
+
+  let mut i = 0;
+  for _index_1 in 0..dim/2{
+    let mut j = 0;
+    for _index_2 in 0..dim/2{
+      partial_transpose_matrix[[i         , j        ]] = upper_left_block[ [i , j] ];
+      partial_transpose_matrix[[i         , j + dim/2]] = upper_right_block_transpose[ [i , j] ];
+      partial_transpose_matrix[[i + dim/2 , j        ]] = lower_left_block_transpose[ [i , j] ];
+      partial_transpose_matrix[[i + dim/2 , j + dim/2]] = lower_right_block[ [i , j] ];
+      j += 1;
+    }
+    i += 1;
+  }
+
+  partial_transpose_matrix
+}
+
+pub fn find_log_negativity(rho: MatrixC64) -> f64{
+  let neg = find_negativity(rho);
+  (2.*neg + 1.).log2()
+}
+
+
+pub fn find_sqr_root_of_matrix(matrix_d: MatrixC64, matrix_s: MatrixC64) -> MatrixC64 {
 
 //Hermitian matrices are always diagonalizable, so we can express rho = S*D*(S^-1),
 //so the sqrt(rho) = S*(D^0.5)*(S^-1). D^0.5 is a matrix of the sqr root of the 
 //elements of D since it is diagonal.
 
-  let (mut eigs, vecs) = rho.eigh(UPLO::Lower).unwrap();
+  let matrix_s_inv = matrix_s.inv().unwrap();
+  let sqrt_matrix_d = matrix_d.mapv(|matrix_d| (matrix_d.re).sqrt());
+  let sqrt_matrix_d_complex = sqrt_matrix_d.map(|f| c64::new(*f, 0.0));
+  let sqrt_product = matrix_s.dot(&sqrt_matrix_d_complex).dot(&matrix_s_inv);
+  sqrt_product
 
+}
 
-  println!("vecs = {}",vecs);
-  let mat_s = vecs.clone();
+pub fn rescale_neg_eigvals(rho: MatrixC64) -> (MatrixC64, MatrixC64) {
+  
+  let (mut eigvals, vecs) = rho.eigh(UPLO::Lower).unwrap();
+  let eig_len = eigvals.len() as i32;
+  println!("eigvals = {}",eigvals);
 
-  let eig_len = eigs.len() as i32;
 
   let mut j = 0;
   for _ctr in 0..eig_len {
-    if eigs[j] < 0.0 {
-      eigs[j] = 0.0;
-      println!("WARNING: The eivenvalue {} was negative, but rounded to 0", eigs[j]);
+    if eigvals[j] < 0.0 {
+      println!("WARNING: While finding the sqrt of a matrix, the eigenvalue {:.32} was negative, but rounded to 0 and rescaled. \n", eigvals[j]);
+      eigvals[j] = 0.0;
+  
       j += 1;
     }
   }
 
-  println!("eigs = {}",eigs);
+  println!("eigvals_new = {}",eigvals);
+  let eig_num = eigvals.len() as i32;
+  let matrix_d_len = eig_num as usize;
+  let mut matrix_d = MatrixC64::zeros((matrix_d_len,matrix_d_len).f());
 
-  let (eig_0 , eig_1, eig_2, eig_3) = (c32::new(eigs[0], 0.0), c32::new(eigs[1], 0.0), c32::new(eigs[2], 0.0), c32::new(eigs[3], 0.0),);
+  let mut i = 0;
+  let eigvals_c64 = eigvals.map(|f| c64::new(*f, 0.0));
 
-  // println!("mat_s = {}", mat_s);
-  let mat_d = array![ [ eig_0 ,              c32::new(0.0, 0.0) , c32::new(0.0, 0.0) , c32::new(0.0, 0.0) ] , 
-                      [ c32::new(0.0, 0.0) , eig_1              , c32::new(0.0, 0.0) , c32::new(0.0, 0.0) ] , 
-                      [ c32::new(0.0, 0.0) , c32::new(0.0, 0.0) , eig_2              , c32::new(0.0, 0.0) ] , 
-                      [ c32::new(0.0, 0.0) , c32::new(0.0, 0.0) , c32::new(0.0, 0.0) , eig_3              ] ];
+  for _num in 0..eig_num{
+    let mut j = 0;
+    for _num_2 in 0..eig_num{
+      if i==j{
+        matrix_d[ [i , j] ] = eigvals_c64[i];
+      }
+      j += 1;
+    }
+    i +=1;
+  }
 
-  // println!("mat_d = {}", mat_d);
-  let det = mat_s.det().unwrap();
-  // println!("det  = {}", det);
-  let mat_s_inv = mat_s.inv().unwrap();
-  // println!("mat_s_inv = {}", mat_s_inv);
+  let matrix_s = vecs;
 
-  // println!("SDS_inv = {}", mat_s.dot(&mat_d).dot(&mat_s_inv));
-  let sqrt_mat_d = mat_d.mapv(|mat_d| (mat_d.re).sqrt());
-  let sqrt_mat_d_complex = sqrt_mat_d.map(|f| c32::new(*f, 0.0));
-  let sqrt_product = mat_s.dot(&sqrt_mat_d_complex).dot(&mat_s_inv);
-  sqrt_product
+  (matrix_d, matrix_s)
+
+  //Used for debugging
+  // let matrix_s_inv = matrix_s.inv().unwrap();
+  // let rho_new = matrix_s.dot(&matrix_d).dot(&matrix_s_inv);
+  // println!("matrix_d = {}", matrix_d);
+  // let tr = rho_new.trace().unwrap();
+  // let rho_rescaled = rho_new.mapv(|rho_new| rho_new/tr);
+  // rho_new
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
